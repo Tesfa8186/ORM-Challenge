baca289f636a0b3324a8361c5a69756327f6c1da
Tesfa8186 <tesfa1978@gmail.com>
2023-02-23 01:41:38 +0800

    Update files

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,86 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    Category.findAll({
+      include: Product,
+    }).then((result) => {
+      res.json(result);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    Category.findByPk(req.params.id, {
+      include: Product,
+    }).then((result) => {
+      res.json(result);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", (req, res) => {
+  // create a new category
+  try {
+    Category.create(req.body).then((result) => {
+      res.json(result);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", (req, res) => {
+  // update a category by its `id` value
+  try {
+    Category.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id,
+        },
+      },
+      {
+        new: true,
+      }
+    ).then((result) => {
+      res.json(result);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  // delete a category by its `id` value
+  try {
+    Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then((result) => {
+      res.json(result);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ README.md
@@ -0,0 +1,36 @@
+# Title : E-Commerce Back End
+
+## Description
+
+Internet retail, also known as **e-commerce**, is the largest sector of the electronics industry.
+E-commerce platforms like Shopify and WooCommerce provide a suite of services to businesses of all sizes.
+
+## User Story
+
+AS A manager at an internet retail company
+I WANT a back end for my e-commerce website that uses the latest technologies
+SO THAT my company can compete with other e-commerce companies.
+
+GIVEN a functional Express.js API
+WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands
+THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application
+THEN my server is started and the Sequelize models are synced to the MySQL database
+WHEN I open API GET routes in Insomnia for categories, products, or tags
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia
+THEN I am able to successfully create, update, and delete data in my database.
+
+## Installation
+
+Installation of this application would be on the terminal using localhost with npm and test for Categories, Products & Tags takes place in Insomnia.
+
+## Links
+
+### [Walkthrough Video of the Application]()
+
+### [The URL GitHub Repository of the Application]()
+
+© 2022 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
